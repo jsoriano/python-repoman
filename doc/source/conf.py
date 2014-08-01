d7459883519bad53a9c26f77fbf7bc57ee39ea60
Jaime Soriano Pastor <jsoriano@tuenti.com>
2014-08-01 17:24:40 +0200

    Mock modules for sphinx doc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -247,3 +247,29 @@ texinfo_documents = [
 #texinfo_show_urls = 'footnote'
 
 keep_warnings = False
+
+# http://read-the-docs.readthedocs.org/en/latest/faq.html
+class Mock(object):
+
+    __all__ = []
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['pygit2', 'hglib', 'sh', 'cffi']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
